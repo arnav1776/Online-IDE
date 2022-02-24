a61447f149047c3eae8498320a083e770806ea74
Arnav Sharma <arnav1776@gmail.com>
2022-02-24 09:54:04 +0530

    patch-1

--- app.js
+++ app.js
@@ -1,52 +1,39 @@
-var compilebtn = document.getElementById('compilebtn');
-var languages = document.querySelector('#languages');
-var textbox = document.getElementById('textbox');
-var outputResult = document.getElementById('outputResult');
-var langCode;
+var lang=document.getElementById("lang");
+var code=document.getElementById("codeArea");
+var output=document.getElementById("output");
+var button=document.getElementById("compile");
 
-languages.addEventListener('click', function()
-{
-    langCode = languages.options[languages.selectedIndex].value;
-})
-
-compilebtn.addEventListener('click', function(){
-
-    var request = new XMLHttpRequest();
-    request.open('POST', 'https://codequotient.com/api/executeCode');
-    request.setRequestHeader("Content-Type", "application/json");
-    request.send(JSON.stringify({code:textbox.value, langId:langCode}));
-
-    request.addEventListener('load', function(event){
-        var codeId = JSON.parse(event.target.responseText)
-        var mainCodeId = codeId.codeId;
-
+function init(lang,code){
+    button.addEventListener("click",function(){
+        var langid=lang.value;
+        var value=code.value;
+        var data={code:value,langId:langid};
+        var convert=JSON.stringify(data);
+        var request=new XMLHttpRequest();
+        request.open("POST","https://codequotient.com/api/executeCode");
+        request.setRequestHeader("Content-Type","application/json");
+        request.send(convert);
         setTimeout(function(){
-        getRequestInterval(mainCodeId)}, 2000);
-    })
-    clearInterval(setTimeout);   
-    })
-
-
-function getRequestInterval(mainCodeId)
-{
-    var request = new XMLHttpRequest();
-
-    request.open('GET',`https://codequotient.com/api/codeResult/${mainCodeId}`);
-    request.setRequestHeader("Content-Type", "application/json");
-    request.send();
-
-    request.addEventListener('load', function(event)
-    {
-        var mainData = JSON.parse(event.target.responseText)
-        mainData = JSON.parse(mainData.data);
-       
-        if(mainData.output !== '')
-        {
-            outputResult.innerText = mainData.output;
-        }
-        else
-        {
-            outputResult.innerText = mainData.errors;
-        }
-    })
+            var response=JSON.parse(request.responseText);
+            result(response.codeId);
+        },2000);
+    });
 }
+
+function result(codeId){
+  var request=new XMLHttpRequest();
+  request.open("GET",`https://codequotient.com/api/codeResult/${codeId}`);
+  request.send();
+  request.addEventListener("load",function(event){
+    var parent=JSON.parse(event.target.responseText);
+    var data=JSON.parse(parent.data);
+    if (data.output!==""){
+      output.innerHTML=data.output;
+    }
+    else{
+      output.innerHTML=data.errors;
+    }
+  });
+}
+
+init(lang,code);
